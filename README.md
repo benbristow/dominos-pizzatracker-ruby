f28eeef374298a1413217f66e034051f360a06ee
Ben Bristow <me@benbristow.co.uk>
2016-04-05 16:37:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Domino's (unofficial) Pizza Tracker
+
+## Why?
+I was bored and waiting for my pizza to arrive. Also, the official pizza tracker web-app takes up too much valuable screen space.
+
+## How to use it (It's simple... really!)
+`ruby dominos_tracker.rb YOUR_PIZZA_ID`
+
+You can get your Pizza ID by visiting the official pizza tracker and taking the ID from the URL.
+Probably only works for UK orders though. (Although would work elsewhere with a tiny bit of tweaking)
+
+## Any gems needed?
+Nope. As long as you have a recent version of Ruby you should be good to go.
---
+++ dominos_tracker.rb
@@ -0,0 +1,82 @@
+require 'open-uri'
+require 'json'
+
+class DominosPizza
+  def initialize(id)
+    @id = id
+    refresh_info
+  end
+
+  def refresh_info
+    begin
+      @info = JSON.parse(open("https://www.dominos.co.uk/pizzaTracker/getOrderDetails?id=#{@id}").read)
+    rescue
+      @info = {"statusId" => -1}
+    end
+  end
+
+  def get_customer_name
+    @info["customerName"] || "Unknown"
+  end
+
+  def get_store_name
+    @info["storeName"] || "Unknown"
+  end
+
+  def get_store_manager_name
+    @info["storeManagerName"] || "Unknown"
+  end
+
+  def get_delivery_driver_name
+    @info["driverName"] || "Unknown"
+  end
+
+  def get_pizza_status
+    refresh_info
+    case @info["statusId"]
+      when -1
+        "is having connection problems or is invalid :("
+      when 1
+        "has been cancelled"
+      when 2
+        "has been collected"
+      when 3
+        "has been delivered by #{get_delivery_driver_name}"
+      when 4
+        "cannot be found"
+      when 5
+        "is in the oven"
+      when 6
+        "is due order processing"
+      when 7
+        "is being prepared"
+      when 8
+        "is being checked for quality"
+      when 9
+        "is out for delivery and is being delivered by #{get_delivery_driver_name}"
+      when 10
+        "is ready for collection"
+      else
+        "has been lost somewhere"
+    end
+  end
+
+end
+
+
+pizza = DominosPizza.new(ARGV[0])
+last_status = ""
+puts "===================================================================================="
+puts "Pizza ID: #{ARGV[0]}"
+puts "Customer Name: #{pizza.get_customer_name}"
+puts "Store: #{pizza.get_store_name}"
+puts "Manager: #{pizza.get_store_manager_name}"
+puts "===================================================================================="
+
+while true
+  if (status = pizza.get_pizza_status != last_status)
+    puts "Your pizza #{pizza.get_pizza_status}."
+    last_status = status
+  end
+  sleep 30
+end
